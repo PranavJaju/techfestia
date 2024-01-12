f398133f089e6f9b21463ac0b050ac5056cf24ec
PranavJaju <pranavjaju45@gmail.com>
2024-01-12 15:07:45 +0530

    team_name

--- app.js
+++ app.js
@@ -22,11 +22,16 @@ app.get("/",async(req,res)=>{
 app.post("/form",async(req,res)=>{
 try{
     const {team_name,problem_id,college,leader_name,email,mobile,place,utr} = req.body;
-    // console.log(req.body);
+    console.log(req.body);
     const result = await student.findOne({$or:[{email:email},{mobile:mobile}]})
     if (result) {
         console.log(result);
         return res.status(401).json({ message: "Some Email/Mobile already registered with us" });
+    }
+    const teamCheck = await student.findOne({team_name:team_name})
+    if(teamCheck){
+        console.log(teamCheck);
+        return res.status(402).json({ message: "Sorry the team name is taken" });
     }
     const Nsave = new student({team_name,problem_id,college,place,utr,email,mobile,leader_name});
    
@@ -34,8 +39,9 @@ try{
     
     res.status(201).json({team,message:"Form Submitted Successfully"});
 }catch(err){
-    res.status(500).json({err});
     console.log(err)
+    res.status(500).json({err});
+    
 }
 })
 app.listen(8080,()=>{
