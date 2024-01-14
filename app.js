5889f23d7b468a102f28be6c2b1ab5dbbfdda383
PranavJaju <pranavjaju45@gmail.com>
2024-01-14 15:59:36 +0530

    File

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const student = require("./models/model");
+const https = require('https');
+const fs = require('fs');
+const path = require("path")
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -44,6 +47,11 @@ try{
     
 }
 })
-app.listen(8080,()=>{
-    console.log("server running");
+const sslServer = https.createServer({
+    key:fs.readFileSync(path.join(__dirname,'./cert/key.pem')),
+    cert:fs.readFileSync(path.join(__dirname,'./cert/cert.pem'))
+},app)
+
+sslServer.listen(8080,()=>{
+    console.log("Secure Server started")
 })
